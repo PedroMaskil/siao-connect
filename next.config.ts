1d1de60369d8441589223c5299fb1ed532023c2e
Pedro Maskil Severino <pedromaskil@hotmail.com>
2026-02-14 17:19:41 -0300

    ajuste

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-import { addDoc, collection } from "firebase/firestore";
 import type { NextConfig } from "next";
-import { db } from "./lib/firebase";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-
-await addDoc(collection(db, "eventos"), {
-  nome: "Evento via código",
-  criadoEm: new Date(),
-});
 export default nextConfig;
